4533bd6165f44ba2fa794f87f09611d86224f36d
Geert van Horrik <geert@catenalogic.com>
2014-12-23 18:31:17 +0100

    Refactored views

--- src/LogViewer/Views/LogNavigatorView.xaml.cs
+++ src/LogViewer/Views/LogNavigatorView.xaml.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LogNavigatorView.xaml.cs" company="Orcomp development team">
+//   Copyright (c) 2008 - 2014 Orcomp development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
 
 namespace LogViewer.Views
 {
--- src/LogViewer/Views/LogRecordsTableView.xaml.cs
+++ src/LogViewer/Views/LogRecordsTableView.xaml.cs
@@ -1,24 +1,32 @@
-﻿namespace LogViewer.Views
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LogRecordsTableView.xaml.cs" company="Orcomp development team">
+//   Copyright (c) 2008 - 2014 Orcomp development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace LogViewer.Views
 {
     using System.Windows.Controls;
 
     /// <summary>
     /// Interaction logic for LogRecordsTableView.xaml
     /// </summary>
-    public partial class LogRecordsTableView 
+    public partial class LogRecordsTableView
     {
-        private GroupStyle _defaultGroupStyle;
+        private readonly GroupStyle _defaultGroupStyle;
 
         public LogRecordsTableView()
         {
             InitializeComponent();
-            _defaultGroupStyle = (GroupStyle)this.FindResource("groupStyle");
+
+            _defaultGroupStyle = (GroupStyle) FindResource("groupStyle");
             InitDataGrid();
         }
 
         private void InitDataGrid()
         {
-            this.Grid.GroupStyle.Add(_defaultGroupStyle);
+            Grid.GroupStyle.Add(_defaultGroupStyle);
         }
     }
 }
