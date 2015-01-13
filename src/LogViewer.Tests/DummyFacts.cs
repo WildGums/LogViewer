ea2a5f93a70ee232c70249305e04d1ca256dd2f2
Geert van Horrik <geert@catenalogic.com>
2015-01-13 13:58:33 +0100

    Added unit test project

---
+++ src/LogViewer.Tests/DummyFacts.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DummyFacts.cs" company="Wild Gums">
+//   Copyright (c) 2014 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace LogViewer.Test
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DummyFacts
+    {
+        #region Methods
+        [Test]
+        public void DummyTest()
+        {
+        }
+        #endregion
+    }
+}
