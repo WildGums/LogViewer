b513f94babb9b984f7a523267ec893177f6fd9e6
Maxim Khomutov <mkhomutov@gmail.com>
2015-04-02 17:46:41 +0300

    LV-5 Show level tooltips only when timestamp is hidden

--- src/LogViewer/Converters/TimestampToStringConverter.cs
+++ src/LogViewer/Converters/TimestampToStringConverter.cs
@@ -3,14 +3,16 @@
 //   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
+
+
 namespace LogViewer.Converters
 {
     using System;
-    using System.Globalization;
     using Catel.MVVM.Converters;
 
     public class TimestampToStringConverter : ValueConverterBase
     {
+        #region Methods
         protected override object Convert(object value, Type targetType, object parameter)
         {
             if (!(value is DateTime))
@@ -21,5 +23,6 @@ namespace LogViewer.Converters
             var dateTimeValue = (DateTime) value;
             return dateTimeValue.ToString(CurrentCulture.DateTimeFormat);
         }
+        #endregion
     }
 }
