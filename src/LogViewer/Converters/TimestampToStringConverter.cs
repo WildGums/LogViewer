941176ab513618ec553c1abf65e8474733451004
Maxim Khomutov <mkhomutov@gmail.com>
2015-04-02 17:07:50 +0300

    LV-5 Added tooltips to the "log level" cell

---
+++ src/LogViewer/Converters/TimestampToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TimestampToStringConverter.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace LogViewer.Converters
+{
+    using System;
+    using System.Globalization;
+    using Catel.MVVM.Converters;
+
+    public class TimestampToStringConverter : ValueConverterBase
+    {
+        protected override object Convert(object value, Type targetType, object parameter)
+        {
+            if (!(value is DateTime))
+            {
+                return null;
+            }
+
+            var dateTimeValue = (DateTime) value;
+            return dateTimeValue.ToString(CurrentCulture.DateTimeFormat);
+        }
+    }
+}
