5963f4bc7669edf7121d48a421800158d5f4de04
Geert van Horrik <geert@catenalogic.com>
2026-05-14 23:45:13 +0200

    Update

--- src/LogViewer/Views/SettingsWindow.xaml.cs
+++ src/LogViewer/Views/SettingsWindow.xaml.cs
@@ -5,6 +5,9 @@
 
     public partial class SettingsWindow
     {
-
+        partial void OnInitializedComponent()
+        {
+            Mode = Catel.Windows.DataWindowMode.OkCancel;
+        }
     }
 }
